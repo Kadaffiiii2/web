5c82aa3fb92a4405437406bfe5a0d640665e3ebc
Kadaffiii <kadaffidaffi25@gmail.com>
2025-03-18 13:14:56 +0700

    Update script.js
    Signed-off-by: Kadaffiii <kadaffidaffi25@gmail.com>

--- script.js
+++ script.js
@@ -9,37 +9,12 @@ document.getElementById("contactForm").addEventListener("submit", function(event
 
     if (name && email && category && message) {
         responseMessage.textContent = `Pesan Anda (${category}) berhasil dikirim!`;
-        responseMessage.style.color = "#4B3619";
+        responseMessage.style.color = "#4CAF50";
 
         // Reset form setelah sukses
         document.getElementById("contactForm").reset();
     } else {
         responseMessage.textContent = "Harap isi semua bidang!";
-        responseMessage.style.color = "#4B3619";
+        responseMessage.style.color = "#FF5733";
     }
 });
-// Tambahkan efek hover pada menu navigasi
-document.querySelectorAll("nav ul li a").forEach(link => {
-    link.addEventListener("mouseover", function() {
-        this.style.color = "#FFD700"; // Warna emas saat hover
-    });
-    function ubahBiodata() {
-    document.getElementById("nama").innerText = "Kadaffi Piliang";
-    document.getElementById("nim").innerText = "220709024";
-    document.getElementById("jurusan").innerText = "Perpustakaan dan Sains Informasi";
-    document.getElementById("fakultas").innerText = "Ilmu Budaya";
-    document.getElementById("kampus").innerText = "Universitas Sumatera Utara";
-    document.getElementById("email").innerText = "kadaffipiliang09@gmail.com";
-    
-    alert("Biodata telah diperbarui!");
-}
-
-    link.addEventListener("mouseout", function() {
-        this.style.color = "white"; // Kembali ke warna default
-    });
-});
-
-// Tampilkan alert saat halaman dimuat
-window.onload = function() {
-    alert("Selamat datang di halaman About Me - Perpustakaan Kadaffi!");
-};
