d3df2496794b5eaa0ea33f33a40a9eb233b1f07f
Kadaffiii <kadaffidaffi25@gmail.com>
2025-03-18 12:25:26 +0700

    Update script.js
    Signed-off-by: Kadaffiii <kadaffidaffi25@gmail.com>

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ document.getElementById("contactForm").addEventListener("submit", function(event
 
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
+    let category = document.getElementById("category").value;
     let message = document.getElementById("message").value;
     let responseMessage = document.getElementById("responseMessage");
 
-    if (name && email && message) {
-        responseMessage.textContent = "Pesan Anda berhasil dikirim!";
+    if (name && email && category && message) {
+        responseMessage.textContent = `Pesan Anda (${category}) berhasil dikirim!`;
         responseMessage.style.color = "#4CAF50";
 
         // Reset form setelah sukses
